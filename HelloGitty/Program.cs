99e872f52c0a0782603233749f1c9592d7a2bd86
marek.flach <marek.flach@VT1-03>
2023-03-06 10:42:04 +0100

    Přidejte soubory projektu.

---
+++ HelloGitty/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] zajmy = { "Hry", "Cestování", "Fotbal" };
+
+Console.WriteLine("Ahoj jsem Marek Flach, chodím na SŠ Educhem a baví mě:");
+
+foreach(var PoleZajmu in zajmy)
+{
+    Console.WriteLine(PoleZajmu);
+}
